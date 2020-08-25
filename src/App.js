a228108332245d7e40dac3e75f16ec7d69adbb48
Andrés Villalobos <agmvilla@gmail.com>
2020-08-25 01:44:22 -0300

    Estado e imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header.js';
 
 /**
  * Componente
  * Propriedade: 
- * Estado
+ * Estado e imutabilidade
  */
 
 function App() {
+    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web'])
+
+    // useState retorna um array com 2 posições
+    // 
+    // 1. Variável com seu valor inicial
+    // 2. Função para atualizarmos esse valor
+
+    function handleAddProject(){
+        setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+
+        console.log(projects);
+    }
+
     return (
         <>
-            <Header title='Homepage' />
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projetos</li>
-                </ul>
             <Header title='Projects' />
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projetos</li>
-                    <li>Login</li>
-                </ul>
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
         </>
     )
 }
