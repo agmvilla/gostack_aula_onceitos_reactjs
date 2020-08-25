2d53c06191f9b8b5a2e97e1f81ab50977655a734
Andrés Villalobos <agmvilla@gmail.com>
2020-08-25 01:32:14 -0300

    Propriedades

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 import Header from './components/Header.js';
 
+/**
+ * Componente
+ * Propriedade: 
+ * Estado
+ */
+
 function App() {
-    return <Header />
+    return (
+        <>
+            <Header title='Homepage' />
+                <ul>
+                    <li>Homepage</li>
+                    <li>Projetos</li>
+                </ul>
+            <Header title='Projects' />
+                <ul>
+                    <li>Homepage</li>
+                    <li>Projetos</li>
+                    <li>Login</li>
+                </ul>
+        </>
+    )
 }
 
 export default App;
